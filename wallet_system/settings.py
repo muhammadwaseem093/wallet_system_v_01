1c32631252dafc4a873f9e6230caf2cf32ebc766
Muhammad Waseem <muhammadwaseem093@gmail.com>
2024-12-04 00:28:07 +0500

    deployment commit

--- wallet_system/settings.py
+++ wallet_system/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -131,6 +132,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 MEDIA_URL = "/media/"
 STATICFILES_DIRS = [
     BASE_DIR / "static",  # For example, if you have a static folder in the root of the project
