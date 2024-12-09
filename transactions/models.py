0cdffb46293017dabd06d5b906e1adc7049afe37
Muhammad Waseem <muhammadwaseem093@gmail.com>
2024-12-09 20:03:30 +0500

    add trasaction app

--- transactions/models.py
+++ transactions/models.py
@@ -1,3 +1,24 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Transaction(models.Model):
+    TRANSACTION_TYPES = (
+        ('sending','Sending'),
+        ('receiving','Receiving'),
+    )
+    
+    STATUS_CHOICES = (
+        ("success","Success"),
+        ("failed","Failed"),
+        ("pending","Pending"),
+    )
+    
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    amount=models.DecimalField(max_digits=10, decimal_places=2)
+    transaction_type = models.CharField(max_length=10, choices=TRANSACTION_TYPES)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES)
+    date = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(Self):
+        return f"{self.user.username} - {self.amount} - {self.transaction_type} - {self.status}"
+    
