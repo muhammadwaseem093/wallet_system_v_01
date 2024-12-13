1f142827c30117075ebf382c6cd9f551c05e8470
Muhammad Waseem <muhammadwaseem093@gmail.com>
2024-12-13 14:36:25 +0500

    add some redfk

--- merchant/urls.py
+++ merchant/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.merchant_dashboard, name='dashboard'),
+    path('dashboard/', views.merchant_dashboard, name='dashboard'),
     # path('transactions/', views.merchant_transactions, name='transactions'),
     # path('transaction/<int:id>/', views.merchant_transaction, name='transaction'),
     # path('transaction/<int:id>/approve/', views.merchant_approve_transaction, name='approve_transaction'),
